854fd13f4dd610aa6d60b780c627f4d291aafabb
williamthomson-dev <williamthomson.dev@outlook.com>
2025-07-05 17:14:54 +0100

    Add Schedule and ScheduleBlock models
    Introduced Schedule and ScheduleBlock classes to represent working hour schedules and their time blocks. These models will support the management of machine maintenance scheduling.

---
+++ MachineMaintenanceScheduler/Features/WorkingHours/Models/Schedule.cs
@@ -0,0 +1,9 @@
+﻿namespace MachineMaintenanceScheduler.Features.WorkingHours.Models
+{
+    public class Schedule
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public List<ScheduleBlock> ScheduleBlocks { get; set; } = new();
+    }
+}
---
+++ MachineMaintenanceScheduler/Features/WorkingHours/Models/ScheduleBlock.cs
@@ -0,0 +1,13 @@
+﻿namespace MachineMaintenanceScheduler.Features.WorkingHours.Models
+{
+    public class ScheduleBlock
+    {
+        public Guid Id { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public string Description { get; set; } = "";
+
+        public Guid ScheduleId { get; set; }
+        public Schedule? Schedule { get; set; } // Navigation property for the parent schedule
+    }
+}
