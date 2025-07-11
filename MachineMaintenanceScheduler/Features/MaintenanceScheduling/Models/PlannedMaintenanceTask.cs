49ef6ebfa4e28cdcfb23ad5d64aa90fefeac509f
williamthomson-dev <williamthomson.dev@outlook.com>
2025-07-11 15:53:11 +0100

    Add PlannedMaintenanceTask model
    Introduces the PlannedMaintenanceTask class to represent scheduled maintenance tasks, including references to Machine and Technician entities.

---
+++ MachineMaintenanceScheduler/Features/MaintenanceScheduling/Models/PlannedMaintenanceTask.cs
@@ -0,0 +1,15 @@
+﻿using MachineMaintenanceScheduler.Features.Machines.Models;
+using MachineMaintenanceScheduler.Features.Technicians.Models;
+
+namespace MachineMaintenanceScheduler.Features.MaintenanceScheduling.Models
+{
+    public class PlannedMaintenanceTask
+    {
+        public Guid Id { get; set; } = Guid.NewGuid();
+        public DateTime MaintenanceScheduledDate { get; set; }
+        public Guid MachineId { get; set; }
+        public Machine Machine { get; set; } = default!;
+        public Guid TechnicianId { get; set; } 
+        public Technician Technician { get; set; } = default!;
+    }
+}
